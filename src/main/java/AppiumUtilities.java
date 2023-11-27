fc74d24a4ffc4a449b40a371daef1cf50a271b63
Svale09 <svalinaivan09+git@gmail.com>
2023-11-27 14:10:05 +0100

    Implemented AppiumUtilities class, added a base PageObject model

---
+++ src/main/java/AppiumUtilities.java
@@ -0,0 +1,19 @@
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+
+import java.io.File;
+
+public class AppiumUtilities {
+    public AppiumDriverLocalService service;
+
+    public AppiumDriverLocalService startAppiumService(String ipAddress, int port){
+        service = new AppiumServiceBuilder()
+                .withAppiumJS(new File("/usr/local/lib/node_modules/appium/build/lib/main.js"))
+                .withIPAddress(ipAddress)
+                .usingPort(port)
+                .build();
+
+        service.start();
+        return service;
+    }
+}
